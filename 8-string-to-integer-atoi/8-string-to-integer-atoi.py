736c332a044f2044a0626686bc8ac63bd6ea9d58
mohammed_algamal <86320895+mohammedalgamal@users.noreply.github.com>
2022-07-28 20:01:32 +0200

    Time: 64 ms (5.47%), Space: 13.5 MB (74.60%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = ''
+        for i in range(len(s)):
+            if s[i] == ' ' and ans == '':
+                continue
+            if s[i].isalpha() or s[i] == '.':
+                break
+            if s[i] == '-':
+                ans += '-'
+                continue
+            if s[i] == '+':
+                ans += '+'
+                continue
+            #if ans == '+-' or ans == '-+':
+                #ans = ''
+                #break
+            j = i
+            while j < len(s) and s[j] >= '0' and s[j] <= '9':
+                ans += s[j]
+                j += 1
+            break
+        #if ans in ['', '-', '+', '-+', '+-']:
+            #ans = 0
+            
+        try: 
+            ans = int(ans)
+        except ValueError:
+            ans = 0
+        if ans < -2 ** 31:
+            ans = -2 ** 31
+        elif ans > (2 ** 31) - 1:
+            ans = (2 ** 31) - 1
+
+        return ans
